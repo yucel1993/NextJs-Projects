464c02c42394064abc1d450b5217e80f51b3b0d1
Burhan <alpagut1993@gmail.com>
2023-09-05 18:29:59 +0200

    carApp

---
+++ carclone/app/(components)/CarCard.tsx
@@ -0,0 +1,29 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+import { CarProps } from './(types)';
+import { calculateCarRent } from '@/utils';
+
+interface CarCardProps{
+    car:CarProps;
+}
+
+const CarCard = ({car}:CarCardProps) => {
+    const { city_mpg, year, make, model, transmission, drive } = car;
+
+    const carRent=calculateCarRent( city_mpg, year)
+  return (
+    <div className='car-card group'>
+        <div className='car-card__content'>
+            <h2 car-card__content-title>{make} {model}</h2>
+        </div>
+        <p className='flex mt-6 text-[32px] font-extrabold'>
+            <span className='self-start text-[14px] font-semibold'>
+                {carRent}
+            </span>
+        </p>
+    </div>
+  )
+}
+
+export default CarCard
