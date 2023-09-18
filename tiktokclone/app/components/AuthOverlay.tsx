9706dfc895dc63bd491f4e565b961a467d9cdfb8
Burhan <alpagut1993@gmail.com>
2023-09-18 22:19:41 +0200

    post-1

---
+++ tiktokclone/app/components/AuthOverlay.tsx
@@ -0,0 +1,30 @@
+
+"use client"
+import { useState } from "react"
+import { AiOutlineClose } from "react-icons/ai"
+import Login from "./auth/Login"
+import Register from "./auth/Register"
+
+export default function AuthOverlay(){
+    const [isRegister , setIsRegister ] = useState<boolean>(true)
+    
+    return(
+        <>
+       <div
+       id="AuthOverlay"
+       className="fixed flex items-center justify-center z-50 top-0 left-0 w-full h-full bg-black bg-opacity-50"   
+       >
+
+        <div className="relative bg-white w-full max-w-[470px] h-[70%] p-4 rounded-lg">
+        <div className="w-full flex justify-end">
+        <button
+        className="p-1.5 rounded-full bg-gray-100">
+        <AiOutlineClose size="26" />
+        </button>
+        </div>
+        {isRegister ? <Register /> : <Login />}
+        </div>
+       </div>
+        </>
+    )
+}
