abf710f1c978726ae4488aa539d0711f2b27c458
Burhan <alpagut1993@gmail.com>
2023-10-08 19:34:55 +0200

    devFlow

---
+++ devflow/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Sign In</div>
+  )
+}
+
+export default page
--- ebayclone/app/hooks/useIsLoading.js
+++
@@ -1,6 +0,0 @@
-const useIsLoading = (bool) => {
-    localStorage.setItem('isLoading', bool)
-    window.dispatchEvent(new Event("storage"));
-}
-
-export default useIsLoading;
