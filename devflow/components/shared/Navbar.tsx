4536355060b8ee4b6a673de541a2f8ab0b73f323
Burhan <alpagut1993@gmail.com>
2023-10-15 15:28:24 +0200

    navbar:finished

--- devflow/components/shared/Navbar.tsx
+++ devflow/components/shared/Navbar.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Theme from './Theme'
+import MobileNav from './MobileNav'
+import GlobalSearch from '../search/GlobalSearch'
 
 const Navbar = () => {
   return (
@@ -18,7 +20,7 @@ const Navbar = () => {
     />
     <p className='h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden'>Dev <span className='text-primary-500'>Flow</span></p>
     </Link>
-    GlobalSearch
+    <GlobalSearch />
     <div className='flex-between gap-5'>
       <Theme/>
     <SignedIn>
@@ -34,7 +36,7 @@ const Navbar = () => {
     />
 
     </SignedIn>
-    MobileNav
+    <MobileNav />
     </div>
     </nav>
   )
