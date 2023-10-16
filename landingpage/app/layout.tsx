8c41b90b90e44bdec52d6762aa514e819d4bafb8
Burhan <alpagut1993@gmail.com>
2023-10-16 12:42:03 +0200

    project:continue

---
+++ landingpage/app/layout.tsx
@@ -0,0 +1,26 @@
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel UI/UX for camping",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
